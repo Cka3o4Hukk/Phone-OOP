58299a353e248a905f2c83aa9410ced8a66876b4
Alex Shomin <alex.sh0min@yandex.com>
2024-04-06 23:41:21 +0700

    Second programm: phone + days

--- test.py
+++ test.py
@@ -8,14 +8,17 @@ class Phone:
     def ring(self):
         print('Дзззззыыыыыыыынь!')
 
+    def call(self, phone_number):
+        print(f'Звоню по номеру {phone_number}! Тип связи - {self.line_type}.')
 
-# Создать экземляр класса Phone.
+    def dial_type_upgrade(self, new_dial_type):
+        self.dial_type = new_dial_type
+
+    # Вот он - магический метод __str__ с пользовательским описанием.
+    def __str__(self):
+        return f'Это {self.line_type} телефон. Набор - {self.dial_type}.'
+
+
 rotary_phone = Phone(dial_type_value='дисковый')
 
-# Вызвать метод ring для экземлпяра rotary_phone. В методе ring() есть
-# единственный параметр self. Передавать его в качестве аргумента не нужно.
-# Python сделает это автоматически.
-rotary_phone.ring()
-
-# Выведется:
-# Дзззззыыыыыыыынь!
+print(rotary_phone)
